5ead52690dbf8327472959aae23398323fb47130
Arnaud Gardelein <arnaud@oscopy.org>
2025-10-31 23:53:35 +0100

    Factorize header processing

--- tab.py
+++ tab.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 import unittest
 
+WRITE_FILES = False
+
 class BaseView:
     DATA_START_COLUMN = None
     INITIAL_CHAPTER_NAME_COLUMN = None
@@ -14,7 +16,7 @@ class BaseView:
     def __init__(self, filename, page, table_number):
         self.data = None
         self.read_data(filename, page, table_number)
-        self.process_header()
+        self.convert_header_to_labels()
         self.merge_nan_lines()
         self.remove_notes_from_chapter_names()
         self.delete_useless_columns()
@@ -29,15 +31,23 @@ class BaseView:
                              )
         self.data = df[table_number]
 
-    def process_header(self):
-        names, drop_list = self._process_header()
+    def convert_header_to_labels(self):
+        drop_list = list(range(self.LAST_HEADER_LINE + 1))
+        names = {}
+        for i, col in self.data.items():
+            s = ' '.join(self.merge_header_cells(col)).title()
+            names[i] = self.remove_notes(s)
+        names = self.fix_labels(names)
         self.data.rename(columns=names, inplace=True)
         self.data.drop(drop_list, inplace=True)
         self.data.reset_index(drop=True, inplace=True)
-        
-    def _process_header(self):
+
+    def fix_labels(self):
         raise NotImplementedError
-        
+
+    def merge_header_cells(self, cells):
+        return cells[:self.LAST_HEADER_LINE+ 1].dropna().astype(str)
+    
     def merge_nan_lines(self):
         drop_list = []
         for i, line in self.data.iterrows():
@@ -93,25 +103,16 @@ class BaseView:
         self.data.iloc[:,self.DATA_START_COLUMN:] = dataset.map(fun)
         self.data = self.data.convert_dtypes(convert_integer=False)
 
-    def set_header(self):
-        names = dict(zip(list(self.data.columns), self.HEADER))
-        self.data.rename(columns=names, inplace=True)
-
 class BalanceGenerale(BaseView):
     DATA_START_COLUMN = 2
     INITIAL_CHAPTER_NAME_COLUMN = 1
+    LAST_HEADER_LINE = 0
 
-    def _process_header(self):
-        header = self.data.iloc[0, :]
-        names = {}
-        for i, h in enumerate(header):
-            if isinstance(h, str):
-                names[i] = self.remove_notes(h).title()
-            else:
-                names[i] = h
+    def fix_labels(self, names):
         names[0] = "Chapitre"
-        drop_list = 0
-        return names, drop_list
+        if not names[2]:
+            names[2] = np.nan
+        return names
 
 
 class VueEnsembleDepenses(BaseView):
@@ -120,20 +121,12 @@ class VueEnsembleDepenses(BaseView):
     LAST_HEADER_LINE = 4
     DROP_COLUMNS = [1]
 
-    def _process_header(self):
-        names = {}
-        for i, col in self.data.items():
-            s = ' '.join(self.merge_header_cells(col)).title()
-            names[i] = self.remove_notes(s)
+    def fix_labels(self, names):
         names[0] = 'Chapitre'
         names[1] = 'Titre Chapitre'
         names[2] = np.nan
-        drop_list = list(range(self.LAST_HEADER_LINE + 1))
-        return names, drop_list
+        return names
 
-    def merge_header_cells(self, cells):
-        return cells[:self.LAST_HEADER_LINE+ 1].dropna()
-    
     def specific_process_data(self):
         self.data.loc[0, 'Chapitre'] = 'Total'
 
@@ -168,22 +161,24 @@ class VueEnsembleDepenses(BaseView):
 
 class DetailArticle(BaseView):
 
-    def _process_header(self):
+    def fix_labels(self, names):
         print(self.data)
-        return None, None
-    
-bgdi = BalanceGenerale('BP_2025_ville.pdf', 17, 1)
-bgdi.data.to_csv('bgdi.csv', float_format='%.2f', index=False)
-bgdf = BalanceGenerale('BP_2025_ville.pdf', 17, 2)
-bgdf.data.to_csv('bgdf.csv', float_format='%.2f', index=False)
-bgri = BalanceGenerale('BP_2025_ville.pdf', 19, 1)
-bgri.data.to_csv('bgri.csv', float_format='%.2f', index=False)
-bgrf = BalanceGenerale('BP_2025_ville.pdf', 19, 2)
-bgrf.data.to_csv('bgrf.csv', float_format='%.2f', index=False)
-vedi = VueEnsembleDepenses('BP_2025_ville.pdf', 21, 1)
-vedi.data.to_csv('vedi.csv', float_format='%.2f', index=False)
-veri = VueEnsembleDepenses('BP_2025_ville.pdf', 23, 1)
-veri.data.to_csv('veri.csv', float_format='%.2f', index=False)
+        return None
+
+if WRITE_FILES:
+    bgdi = BalanceGenerale('BP_2025_ville.pdf', 17, 1)
+    bgdi.data.to_csv('bgdi.csv', float_format='%.2f', index=False)
+    bgdf = BalanceGenerale('BP_2025_ville.pdf', 17, 2)
+    bgdf.data.to_csv('bgdf.csv', float_format='%.2f', index=False)
+    bgri = BalanceGenerale('BP_2025_ville.pdf', 19, 1)
+    bgri.data.to_csv('bgri.csv', float_format='%.2f', index=False)
+    bgrf = BalanceGenerale('BP_2025_ville.pdf', 19, 2)
+    bgrf.data.to_csv('bgrf.csv', float_format='%.2f', index=False)
+    vedi = VueEnsembleDepenses('BP_2025_ville.pdf', 21, 1)
+    vedi.data.to_csv('vedi.csv', float_format='%.2f', index=False)
+    veri = VueEnsembleDepenses('BP_2025_ville.pdf', 23, 1)
+    veri.data.to_csv('veri.csv', float_format='%.2f', index=False)
+
 #dadi = DetailArticle('BP_2025_ville.pdf', 25, 1)
 
 print('-'*50, 'Done')
@@ -194,9 +189,14 @@ class test_bg(unittest.TestCase):
     def convert_data(self, data):
         return data.convert_dtypes(convert_integer=False)
 
-    def _test_equals(self, act, ref):
+    def _test_equals(self, act, ref, show=False):
         actual = self.convert_data(act)        
         reference = self.convert_data(ref)
+        if show:
+            print(actual)
+            print(reference)
+            print(actual.dtypes)
+            print(reference.dtypes)
         return self.assertTrue(actual.equals(reference))
     
     def test_balance_generale_depenses_invest(self):
